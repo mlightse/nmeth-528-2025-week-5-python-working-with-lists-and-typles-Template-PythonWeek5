ee7d9d1351451a0d4cdffc109bd1ca571731784c
mlightse <mlightse@uw.edu>
2025-03-11 13:03:40 -0700

    Create Malaysia_Lightsey.py
    revision with .py

---
+++ Malaysia_Lightsey.py
@@ -0,0 +1,16 @@
+List = ["eggs", "milk", "bread", "meat", juice", "rice"]
+print("The items listed are:", List)
+print("The first two listed are:", List[0], List[1])
+print("The middle two listed are:", List[2], List[3])
+print("The first and last two listed are:", List[0], List[-1])
+
+
+Menu = (omelette, pancakes, bagel, orange_juice, sausage)
+print("OG menu:")
+for item in menu:
+  print(item)
+
+Revised_menu = (omelette, pancakes, sausage, mimosa, bacon)
+print("Revised_Menu:")
+for item in Revised_menu:
+  print(item)  
